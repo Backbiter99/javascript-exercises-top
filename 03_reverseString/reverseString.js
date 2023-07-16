4224e0a3991d65b69bfb171974607b21640d45ed
Backbiter99 <prajwaljha@gmail.com>
2023-07-16 16:36:28 +0530

    completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let isAnswer = "";
+  let i = 0;
+  while (i < string.length) {
+    isAnswer += string[string.length - i - 1];
+    i++;
+  }
+  return isAnswer;
 };
 
 // Do not edit below this line
