f74a30c56943f9745f7a5ab665887edfdab418d3
Backbiter99 <prajwaljha@gmail.com>
2023-07-16 22:46:37 +0530

    completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...args) {
+  let array = [];
+  for (let i = 0; i < arguments[0].length; i++) {
+    let check = true;
+    for (let j = 1; j < arguments.length; j++) {
+      if (arguments[0][i] === arguments[j]) {
+        check = false;
+      }
+    }
+    if (check) {
+      array.push(arguments[0][i]);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
