e08297a27c2a6a005f5db21c97621050331e5b7b
Backbiter99 <prajwaljha@gmail.com>
2023-07-17 18:25:55 +0530

    completed leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (!Number.isInteger(year)) return false;
+  if (year % 4) {
+    return false;
+  } else {
+    if (year % 100) {
+      return true;
+    } else {
+      if (year % 400) {
+        return false;
+      } else return true;
+    }
+  }
 };
 
 // Do not edit below this line
