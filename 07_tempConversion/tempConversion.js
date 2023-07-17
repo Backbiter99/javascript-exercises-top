ee5f9c8aa59ab30121ecc806c4060a6595d2a28a
Backbiter99 <prajwaljha@gmail.com>
2023-07-17 18:43:41 +0530

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  const tempC = ((tempF - 32) * 5) / 9;
+  if(Number.isInteger(tempC)) return tempC;
+  return +tempC.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  const tempF = (9 * tempC) / 5 + 32;
+  if(Number.isInteger(tempC)) return tempF;
+  return +tempF.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
