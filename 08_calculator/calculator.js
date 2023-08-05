a174cd0ea74521dbf48da1677b695d00bd475741
Backbiter99 <prajwaljha@gmail.com>
2023-08-05 16:30:03 +0530

    completed calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((totalSum, element) => totalSum + element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function () {
+  let ans = 1;
+  for (let i = 0; i < arguments.length; i++) {
+    ans = ans * arguments[i];
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let x = 1,
+    ans = 1;
+  while (x <= a) {
+    ans = ans * x;
+    x++;
+  }
+  return ans;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
