74f172af586e6f93cf110ca5531409ffe5638392
Backbiter99 <prajwaljha@gmail.com>
2023-08-05 18:38:03 +0530

    completed palindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strForwards = str
+    .split("")
+    .filter((element) => {
+      if (element === " ") return false;
+      if (
+        (element >= "a" && element <= "z") ||
+        (element >= "A" && element <= "Z") ||
+        (element >= 0 && element <= 9)
+      ) {
+        return true;
+      }
+    })
+    .join("")
+    .toLowerCase();
+  let strBackwards = strForwards.split("").reverse().join("");
+  if (strForwards === strBackwards) return true;
+  else return false;
 };
 
 // Do not edit below this line
