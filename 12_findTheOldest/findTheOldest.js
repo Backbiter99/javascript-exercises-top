2065b89bdf6d13d3947de3d99caa9e64f5d596ac
Backbiter99 <prajwaljha@gmail.com>
2023-08-06 22:11:30 +0530

    completed findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.sort((a, b) => {
+    if (!a["yearOfDeath"]) {
+      a["yearOfDeath"] = new Date().getFullYear();
+    }
+    if (!b["yearOfDeath"]) {
+      b["yearOfDeath"] = new Date().getFullYear();
+    }
+    const firstGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    return firstGuy > nextGuy ? -1 : 1;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
